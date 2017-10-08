c01a97cd91cf6d234eaee667040dd64486c44d3c
Yu Ishihara <yuishihara1225@gmail.com>
2017-10-08 18:26:18 +0900

    Add template cpp file for atcoder

---
+++ AtCoder/newcode.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+USAGE="usage: ./newcode.sh [ProblemNumber] [ProblemClass], example: ./newcode.sh practice 2"
+
+if test $# -ge 3 ; then
+    echo too much arguments
+    echo $USAGE
+    exit 1
+elif test $# -le 1 ; then
+    echo too few arguments
+    echo $USAGE
+    exit 1
+fi
+
+TEMPLATE_FILE=../Template/Template.cpp
+CPP_SUFFIX=.cpp
+CPP_FILENAME=$1$2$CPP_SUFFIX
+cp $TEMPLATE_FILE $CPP_FILENAME
+exit 0
