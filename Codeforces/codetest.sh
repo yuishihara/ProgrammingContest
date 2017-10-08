ed3b1b5648eab5ebf08dd6119ffec2f30ff99b38
Yu Ishihara <yuishihara1225@gmail.com>
2017-10-08 17:34:17 +0900

    Fix check whether directory actually exists' gits

--- Codeforces/codetest.sh
+++ Codeforces/codetest.sh
@@ -2,17 +2,19 @@
 USAGE="usage: ./codetest.sh [ProblemNumber] [ProblemClass], example: ./codetest.sh 100 A"
 
 if test $# -ge 3 ; then
- echo too much arguments
- echo $USAGE
- exit 1
+    echo too much arguments
+    echo $USAGE
+    exit 1
 elif test $# -le 1 ; then
- echo too few arguments
- echo $USAGE
- exit 1
+    echo too few arguments
+    echo $USAGE
+    exit 1
 fi
 
 #Create folder and move there to not mess up the directory
-mkdir $1
+if ! [ -d $1 ]; then
+    mkdir $1
+fi
 CPP_SUFFIX=.cpp
 CPP_FILENAME=$1$2$CPP_SUFFIX
 cp $CPP_FILENAME $1
