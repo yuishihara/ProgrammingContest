c291b93949ad32213e96ff6ed5647501041d9179
Yu Ishihara <yuishihara1225@gmail.com>
2017-10-08 18:25:58 +0900

    Add code submit/test script for atcoder

---
+++ AtCoder/codetest.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+USAGE="usage: ./codetest.sh [ProblemNumber] [ProblemClass], example: ./codetest.sh practice 2"
+
+if test $# -ge 3 ; then
+    echo too much arguments
+    echo $USAGE
+    exit 1
+elif test $# -le 1 ; then
+    echo too few arguments
+    echo $USAGE
+    exit 1
+fi
+
+#Create folder and move there to not mess up the directory
+if ! [ -d $1 ]; then
+    mkdir $1
+fi
+CPP_SUFFIX=.cpp
+CPP_FILENAME=$1$2$CPP_SUFFIX
+cp $CPP_FILENAME $1
+cd $1
+../oj.py --atcoder -i $CPP_FILENAME $1 $2
+exit 0
