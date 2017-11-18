def7b0944acccd16ef23c2a4d04136fe9eccc0d2
Yu Ishihara <yuishihara1225@gmail.com>
2017-11-18 22:55:05 +0900

    Update cpp template file

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -31,6 +31,8 @@ using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<string> vs;
 
 template<typename T>
 ostream& operator<<(ostream& os, const vector<T>& vec){
@@ -65,6 +67,53 @@ long long gcd(long long a, long long b){
 
 long long lcm(long long a, long long b){
     return (a / gcd(a, b)) * b;
+}
+
+long long mul(const long long& a, const long long& b, const long long& mod) {
+    return ((a % mod) * (b % mod)) % mod;
+}
+
+long long power(const long long& x, const long long& y, const long long& mod) {
+    if (y == 0) {
+        return 1;
+    } else if (y == 1) {
+        return x % mod;
+    } else {
+        long long value = power(x, y / 2, mod);
+        if (y % 2 == 0) {
+            return mul(value, value, mod);
+        } else {
+            return mul(value, value, mod) * x % mod;
+        }
+    }
+}
+
+long long div(const long long& a, const long long& b, const long long& mod) {
+    return mul(a, power(b, mod - 2, mod), mod);
+}
+
+map<long long, long long> factorials;
+long long factorial(const long long& n, const long long& mod) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    if (factorials[n] != 0) {
+        return factorials[n];
+    }
+    factorials[n] = n * factorial(n - 1, mod) % mod;
+    return factorials[n] % mod;
+}
+
+long long combination(const long long& n, const long long& x, const long long& mod) {
+    long long numerator = 1;
+    long long denominator = 1;
+    repeat(i, x) {
+        numerator *= (n - i) % mod;
+        numerator %= mod;
+        denominator *= (i + 1) % mod;
+        denominator %= mod;
+    }
+    return div(numerator, denominator, mod);
 }
 
 int main(){
