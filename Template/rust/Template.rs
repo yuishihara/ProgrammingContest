4293086a4d0779cdc82737dc87425fd5da0c4e6f
Yu Ishihara <yuishihara1225@gmail.com>
2019-08-03 11:41:56 +0900

    Update template

---
+++ Template/rust/Template.rs
@@ -0,0 +1,18 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>().split_whitespace()
+        .map(|e| e.parse().ok().unwrap()).collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+
+fn main() {
+
+}
